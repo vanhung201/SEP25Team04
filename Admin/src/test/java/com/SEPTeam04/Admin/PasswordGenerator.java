6d1bd8a2eeeab64fd908e422d7c33527df6478f7
Hoai-fashionality <hoai0209tran@gmail.com>
2022-07-24 13:19:32 +0700

    Hoai update code 24/7'

--- Admin/src/test/java/com/SEPTeam04/Admin/PasswordGenerator.java
+++ Admin/src/test/java/com/SEPTeam04/Admin/PasswordGenerator.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordGenerator {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "thanhhoai";
+        String rawPassword = "vandat";
         String encodedPassword = encoder.encode(rawPassword);
 
         System.out.println(encodedPassword);
