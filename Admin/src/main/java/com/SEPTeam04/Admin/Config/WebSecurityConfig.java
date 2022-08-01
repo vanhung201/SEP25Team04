c01dcea661aa88318bb303fd35eab23f46c63d9f
vanhung201 <vanhung.work@gmail.com>
2022-08-01 12:37:12 +0700

    HUNG UPDATE CODE

--- Admin/src/main/java/com/SEPTeam04/Admin/Config/WebSecurityConfig.java
+++ Admin/src/main/java/com/SEPTeam04/Admin/Config/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/assets/**", "/css/**", "/img/**", "/js/**", "/vendor/**").permitAll()
+                .antMatchers("/assets/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
