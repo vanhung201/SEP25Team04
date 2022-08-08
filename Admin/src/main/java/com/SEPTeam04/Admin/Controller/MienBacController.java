b10a5c6fe382cf95b48136529a6528ec90e7936f
vanhung201 <vanhung.work@gmail.com>
2022-08-08 17:37:05 +0700

    HUNG UPDATE LAI GIAO DIEN - NAV

--- Admin/src/main/java/com/SEPTeam04/Admin/Controller/MienBacController.java
+++ Admin/src/main/java/com/SEPTeam04/Admin/Controller/MienBacController.java
@@ -1,15 +1,15 @@
 package com.SEPTeam04.Admin.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MienBacController {
 
     @GetMapping("/xosomienbac")
-    public String showMienBac(Model model) {
+    public String showMienBac() {
 
         return "/index";
     }
+
 }
